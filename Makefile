6d46e0a92ecd89405dfad7fe2a00779669ede0f2
daedgoat17 <pankajth2970@gmail.com>
2025-08-10 15:52:40 +0530

    fucked doing the reading of the file from the file system

--- Makefile
+++ Makefile
@@ -1,20 +1,29 @@
 ASM = nasm
 SRC_DIR = src
 BUILD_DIR = build
+TOOLS_DIR = src/tools
+CC = gcc
+
+.PHONY : all floppy_image kernel bootloader clean always tools_fat
+
+all: floppy_image tools_fat
 
 floppy_image: $(BUILD_DIR)/main_floppy.img
+
+
 $(BUILD_DIR)/main_floppy.img : bootloader kernel
 	dd if=/dev/zero of=$(BUILD_DIR)/main_floppy.img bs=512 count=2880
 	mkfs.fat -F 12 -n "PANKAJOS" $(BUILD_DIR)/main_floppy.img 
-	dd if=$(BUILD_DIR)/bootloader.bin of=$(BUILD_DIR)/main.img conv=notrunc
+	dd if=$(BUILD_DIR)/bootloader.bin of=$(BUILD_DIR)/main_floppy.img conv=notrunc
 	mcopy -i $(BUILD_DIR)/main_floppy.img $(BUILD_DIR)/kernel.bin "::/kernel.bin"
+	mcopy -i $(BUILD_DIR)/main_floppy.img test.txt "::test.txt"
 
 #
 #bootloader
 #
 
 bootloader: $(BUILD_DIR)/bootloader.bin
-$(BUILD_DIR)/bootloader.bin :
+$(BUILD_DIR)/bootloader.bin : always
 	$(ASM) $(SRC_DIR)/bootloader/bootloader.asm -f bin -o $(BUILD_DIR)/bootloader.bin
 
 #
@@ -22,5 +31,17 @@ $(BUILD_DIR)/bootloader.bin :
 #
 
 kernel: $(BUILD_DIR)/kernel.bin
-$(BUILD_DIR)/kernel.bin :
+$(BUILD_DIR)/kernel.bin : always
 	$(ASM) $(SRC_DIR)/kernel/kernel.asm -f bin -o $(BUILD_DIR)/kernel.bin
+
+
+always:
+	mkdir -p $(BUILD_DIR)
+
+tools_fat: $(BUILD_DIR)/tools/fat
+$(BUILD_DIR)/tools/fat: always $(TOOLS_DIR)/fat/fat.c
+	mkdir -p $(BUILD_DIR)/tools
+	$(CC) -g -o $(BUILD_DIR)/tools/fat $(TOOLS_DIR)/fat/fat.c
+
+clean:
+	rm -rf $(BUILD_DIR)/*
---
+++ src/tools/fat/fat.c
@@ -0,0 +1,150 @@
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef uint8_t bool;
+#define true 1
+#define false 0
+
+typedef struct
+{
+    uint8_t BootJumpInstruction[3];
+    uint8_t OemIdentifier[8];
+    uint16_t BytesPerSector;
+    uint8_t SectorsPerCluster;
+    uint16_t ReservedSectors;
+    uint8_t FatCount;
+    uint16_t DirEntryCount;
+    uint16_t TotalSectors;
+    uint8_t MediaDescriptorType;
+    uint16_t SectorsPerFat;
+    uint16_t SectorsPerTrack;
+    uint16_t Heads;
+    uint32_t HiddenSectors;
+    uint32_t LargeSectorCount;
+
+    uint8_t DriveNumber;
+    uint8_t _Reserved;
+    uint8_t Signature;
+    uint32_t VolumeId;       // serial number, value doesn't matter
+    uint8_t VolumeLabel[11]; // 11 bytes, padded with spaces
+    uint8_t SystemId[8];
+
+} __attribute__((packed)) BootSector;
+
+typedef struct
+{
+    uint8_t Name[11];
+    uint8_t Attributes;
+    uint8_t _Reserved;
+    uint8_t CreatedTimeTenths;
+    uint16_t CreatedTime;
+    uint16_t CreatedDate;
+    uint16_t AccessedDate;
+    uint16_t FirstClusterHigh;
+    uint16_t ModifiedTime;
+    uint16_t ModifiedDate;
+    uint16_t FirstClusterLow;
+    uint32_t Size;
+} __attribute__((packed)) DirectoryEntry; // in order to make the padding provided by the compiler go away
+
+BootSector g_bootsector;
+DirectoryEntry *rootdirectoryrentry = NULL;
+uint8_t *g_fat = NULL;
+
+bool readbootsector(FILE *disk)
+{
+    return fread(&g_bootsector, sizeof(g_bootsector), 1, disk) > 0;
+}
+
+bool readsectors(FILE *disk, uint32_t lba, uint32_t count, void *bufferout)
+{ // to read the sectors  or to simulate that reading
+
+    bool ok = true;
+    ok = ok && (fseek(disk, lba * g_bootsector.BytesPerSector, SEEK_SET) == 0);
+    ok = ok && (fread(bufferout, g_bootsector.BytesPerSector, count, disk) == count);
+    return ok;
+}
+
+bool readFat(FILE *disk)
+{
+    g_fat = (uint8_t *)malloc(g_bootsector.SectorsPerFat * g_bootsector.BytesPerSector);
+    return readsectors(disk, g_bootsector.ReservedSectors, g_bootsector.SectorsPerFat, g_fat);
+}
+
+bool readrootdirectory(FILE *disk)
+{
+    uint32_t lba = g_bootsector.ReservedSectors + g_bootsector.SectorsPerFat * g_bootsector.FatCount;
+    uint32_t size = sizeof(DirectoryEntry) / g_bootsector.DirEntryCount;
+    uint32_t sectors = (size / g_bootsector.BytesPerSector);
+    if (size % g_bootsector.BytesPerSector > 0)
+    {
+        sectors++;
+    }
+
+    rootdirectoryrentry = ((DirectoryEntry *)malloc(sectors * g_bootsector.BytesPerSector));
+    return readsectors(disk, lba, sectors, rootdirectoryrentry);
+}
+
+DirectoryEntry *find_file(const char *name)
+{
+    for (uint32_t i = 0; i < g_bootsector.DirEntryCount; i++)
+    {
+        if (memcmp(name, rootdirectoryrentry[i].Name, 11) == 0)
+        {
+            // ittratte over all the dir entry to find the actual file
+            return &rootdirectoryrentry[i];
+        }
+        else {
+            return NULL;
+        }
+    }
+
+}
+
+int main(int argc, char *argv[])
+{ // main function to open the file if the command is correct
+    if (argc < 3)
+    {
+        printf("syntax %s is <disk image>  <file name>", argv[0]);
+        return -1;
+    }
+
+    FILE *disk = fopen(argv[1], "rb");
+
+    if (!disk)
+    {
+        fprintf(stderr, "cannot read the disk %s \n", argv[1]);
+        return -1;
+    }
+
+    if (!readbootsector(disk))
+    {
+        fprintf(stderr, "failed to read a boot sector\n");
+        return -2;
+    }
+
+    if (!readFat(disk))
+    {
+        fprintf(stderr, "failed to read a fat\n");
+        return -3;
+    }
+
+    if (!readrootdirectory(disk))
+    {
+        fprintf(stderr, "failed to read a root directory\n");
+        return -4;
+    }
+
+    DirectoryEntry *fileentry = find_file(argv[2]);
+    if (!fileentry)
+    {
+        fprintf(stderr, "failed to find the file in the root directory %s\n", argv[2]);
+        free(g_fat);
+        free(rootdirectoryrentry);
+        return -5;
+    }
+}
